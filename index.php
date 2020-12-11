ec58aef6bfb1534073f5edc1abedaff13c67c765
mook <ji.nongnapat@gmail.com>
2020-12-12 02:40:23 +0700

    update index and tracking

--- index.php
+++ index.php
@@ -15,34 +15,38 @@
 
 </head>
 <body>
-	<div class="row">
-		<div class="container">
-			<div align="center">
-				<div class="card shadow p-3 mb-5 h-100 bg-white rounded align-items-center">
-					<div class="card-body">
-						<h1>Shipping System</h1>
-						<div class="form-group">
-							<div class="input-group mt-3">
-								<div class="input-group-prepend">
-									<div class="input-group-text"><i class="fas fa-user"></i></div>
-								</div>
-								<input class="form-control" type="text" name="" value="" placeholder="">
-							</div>
-							<div class="input-group mt-3">
-								<div class="input-group-prepend">
-									<div class="input-group-text"><i class="fas fa-key"></i></div>
-								</div>
-								<input class="form-control" type="password" name="" value="" placeholder="">
-							</div>
-							<button class="btn btn-primary mt-3" type="" id="test_connect">เข้าสู่ระบบ</button>
+	<style>
+		.login-box {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+		}
+	</style>
+
+	<div class="login-box">
+		<div class="card shadow p-3 mb-5 bg-white rounded">
+			<div class="card-body">
+				<h1>Shipping System</h1>
+				<div class="form-group">
+					<div class="input-group mt-3">
+						<div class="input-group-prepend">
+							<div class="input-group-text"><i class="fas fa-user"></i></div>
 						</div>
+						<input class="form-control" type="text" name="" value="" placeholder="">
 					</div>
+					<div class="input-group mt-3">
+						<div class="input-group-prepend">
+							<div class="input-group-text"><i class="fas fa-key"></i></div>
+						</div>
+						<input class="form-control" type="password" name="" value="" placeholder="">
+					</div>
+					<button class="btn btn-primary mt-3" type="" id="test_connect">เข้าสู่ระบบ</button>
 				</div>
 			</div>
-			<p align="center">Copyright 2020 Shipping System by <a href="#">Dev-Team</a></p>
 		</div>
+		<p align="center">Copyright 2020 Shipping System by <a href="#">Dev-Team</a></p>
 	</div>
-
 </body>
 </html>
 
