326a190657af136e417fba1a50ca4b81bba71138
Chutipas Borsub <chutipas27316@gmail.com>
2021-01-02 22:55:02 +0700

    เพิ่มตาราง sender, แก้ API add product ให้เพิ่มข้อมูล senderเข้าdb

---
+++ api/function/manage_sender.php
@@ -0,0 +1,31 @@
+<?php
+// ini_set('display_errors',1);
+// error_reporting(E_ALL);
+
+include("../main_class/connect_db.php");
+include("../main_class/auth.php");
+include("../main_class/lib_manage_sender.php");
+
+$mysql = new Main_db;
+$auth = new Auth;
+$mng_sender = new MNG_Sender;
+
+$mysql->Connect_db();
+$mysql->SetCharacter();
+$cmd = isset($_POST["command"]) ? $_POST["command"] : "";
+
+if ($cmd != "") {
+
+	if ($cmd == "get_sender") {
+		$permission = $auth->AuthPermission();
+		if($permission['permission']){
+			$result = $mng_sender->GetSender($_POST);
+		}else{
+			$result = array('status' => 500, 'err_msg' => $permission['msg']);
+		}
+		echo json_encode($result);
+		$mysql->Close_db();
+		exit();
+	}
+}
+?>
