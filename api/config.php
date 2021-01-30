65f4602cdd0834c95168c9a7871c6a1ef17cdd4d
Chutipas Borsub <bo.chutipas@gmail.com>
2021-01-31 02:54:44 +0700

    no message

--- api/config.php
+++ api/config.php
@@ -2,16 +2,12 @@
 
 //Database connect============
 
-// define("DB_HOST",'localhost');
-// define("DB_USERNAME",'root');
-// define("DB_PASSWORD",'mysql');
-// define("DB_NAME",'shipping_db');
-
 define("DB_HOST",'localhost');
-define("DB_USERNAME",'id16031240_shippingdev');
-define("DB_PASSWORD",'N@iShippingProd1');
-define("DB_NAME",'id16031240_shipping_db');
+define("DB_USERNAME",'root');
+define("DB_PASSWORD",'mysql');
+define("DB_NAME",'shipping_db');
 
+define("AUTH_CHECK_SESSION", false);
 
 //SESSION Config==============
 
